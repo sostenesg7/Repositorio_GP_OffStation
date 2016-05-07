5403d253ed9267c45252597d762b59e2d65b4a04
luanupe <luan13augusto@hotmail.com>
2016-05-07 17:28:20 -0300

    Editando funcionários...

--- ProjetoGP_OffStation/src/projetos/gerencia/negocio/funcionario/Atendente.java
+++ ProjetoGP_OffStation/src/projetos/gerencia/negocio/funcionario/Atendente.java
@@ -3,7 +3,7 @@ package projetos.gerencia.negocio.funcionario;
 public class Atendente extends Funcionario {
     
     public Atendente(int cpf, int tipo, String nome, String sobrenome, String email, String senha) {
-        super(0, 0, null, null, null, null);
+        super(cpf, tipo, nome, sobrenome, email, senha);
     }
     
 }
