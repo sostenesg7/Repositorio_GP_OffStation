f4fcec6f59e45cce6a896aa39e51db616d37ea1a
Sostenesg7 <sostenesd7@hotmail.com>
2016-05-07 14:39:43 -0300

    Adicionada nova interface Gráfica de Login. Usando JavaFX.

--- ProjetoGP_OffStation/src/projetos/gerencia/apresentacao/Main.java
+++ ProjetoGP_OffStation/src/projetos/gerencia/apresentacao/Main.java
@@ -1,11 +1,32 @@
 package projetos.gerencia.apresentacao;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import projetos.gerencia.apresentacao.gui.FrameLogin;
 
-public class Main {
+public class Main extends Application{
 
+     @Override
+    public void start(Stage stage) throws Exception {
+    
+        //System.out.println(getClass().getResource("ui/FXMLDocument.fxml"));
+        
+        Parent root = FXMLLoader.load(getClass().getResource("ui/FXML_FormLogin.fxml"));
+                
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("LOGIN");
+        
+        stage.show();
+          
+    }
+    
     public static void main(String[] args) {
-        new FrameLogin();
+       launch(args);
     }
 
 }
