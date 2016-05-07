5b362454c5e1243e7cd8134e3ae3881a59253050
DaniloMessias <Usurio@DaniloPC>
2016-05-07 15:13:46 -0300

    essa é a main

--- ProjetoGP_OffStation/src/projetos/gerencia/apresentacao/Main.java
+++ ProjetoGP_OffStation/src/projetos/gerencia/apresentacao/Main.java
@@ -3,7 +3,7 @@ package projetos.gerencia.apresentacao;
 import projetos.gerencia.apresentacao.gui.FrameLogin;
 
 public class Main {
-
+    //essa é a main
     public static void main(String[] args) {
         new FrameLogin();
     }
